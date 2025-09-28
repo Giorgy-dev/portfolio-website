980d60124fb4293eb756dca2f7b752b0b33e2746
Marco <marco.gorgetti.2@gmail.com>
2025-09-28 15:04:59 +0200

    if 404 then home

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  // if 404 then go to home automatically
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+  onMount(() => {
+    goto("/");
+  });
+</script>
